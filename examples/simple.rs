026a8b25fa90ff8422284510cbc8d8255edcdbca
Daniel P <daniel@pont.in>
2023-01-20 20:59:00 +0000

    Add colour changing to simple example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,10 +1,15 @@
+use core::time;
+
 use rgb::RGB8;
 use unicorn_hat_mini::UnicornHATMini;
 
 fn main() -> Result<(), unicorn_hat_mini::UnicornError>{
     let mut uni = UnicornHATMini::default();
     uni.set_brightness(0.1)?;
-    uni.set_all(RGB8{r:255, g:255, b:255});
-    uni.show();
-    Ok(())
+    let rgb = 100;
+    loop {
+        uni.set_all(RGB8{r:rgb, g:rgb, b:rgb});
+        uni.show();
+        std::thread::sleep(time::Duration::from_millis(16));
+    }
 }
