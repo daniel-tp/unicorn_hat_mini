74129344bdaca8cb2d9c98396d7f45d259b82eca
Daniel P <daniel@pont.in>
2023-01-20 20:59:48 +0000

    Remove brightness from simple example

--- examples/simple.rs
+++ examples/simple.rs
@@ -5,11 +5,11 @@ use unicorn_hat_mini::UnicornHATMini;
 
 fn main() -> Result<(), unicorn_hat_mini::UnicornError>{
     let mut uni = UnicornHATMini::default();
-    uni.set_brightness(0.1)?;
-    let rgb = 100;
+    let mut rgb = 100;
     loop {
         uni.set_all(RGB8{r:rgb, g:rgb, b:rgb});
         uni.show();
+        rgb+=1;
         std::thread::sleep(time::Duration::from_millis(16));
     }
 }
